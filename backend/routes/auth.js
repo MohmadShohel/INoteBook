690c82f1226a8be3399e0ee9c8a381392cae5e00
MohmadShohel <mohmad.shohel2021@vitstudent.ac.in.com>
2023-10-03 16:26:21 +0530

    jwttokensUsed

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,28 +2,42 @@ const express = require("express");
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "bu4yufb45784yo8vy24v&*$#";
 
 // Create a User using: POST "/api/auth/". Doesn't require Auth
+
 router.post(
-  "/",
+  "/createUser",
   [
-    body("email", "Enter a valid email").isEmail(),
-    body("name", "Enter a valid name minimum 5 letters").isLength({ min: 5 }),
-    body("password", "Enter a valid name minimum 5 letters").isLength({
+    body("email").isEmail(),
+    body("name").isLength({ min: 5 }),
+    body("password").isLength({
       min: 5,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     console.log(req.body);
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
+    const salt = await bcrypt.genSalt(10);
+    const secPassword = await bcrypt.hash(req.body.password, salt);
+    const user = await User.create({
       name: req.body.name,
-      password: req.body.password,
+      password: secPassword,
       email: req.body.email,
-    }).then((user) => res.json(user));
+    });
+    const data = {
+      user: {
+        id: user.id,
+      },
+    };
+    const authToken = jwt.sign(data, JWT_SECRET);
+    res.json({ authToken });
   }
 );
 
